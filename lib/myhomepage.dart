6abc73547f26da8cdaa9186db15f2f6c4a602426
sinan <sinanalagoz74@gmail.com>
2022-08-03 12:14:42 +0300

    78

--- lib/myhomepage.dart
+++ lib/myhomepage.dart
@@ -15,43 +15,67 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   String fileString = "";
   String fileName = "";
-  double? _progress;
-  UploadTask? uploadTask;
+  // double? _progress;
+  // UploadTask? uploadTask;
+
+  // void singleFile() async {
+  //   FilePickerResult? result = await FilePicker.platform.pickFiles();
+
+  //   if (result != null) {
+  //     File file = File(result.files.single.path!);
+
+  //     setState(() {
+  //       fileString = result.files.single.path!.toString();
+  //       //fileExtension = result.files.single.extension!.toString();
+  //       fileName = result.files.single.name.toString();
+  //       print(fileName);
+  //       // OpenFile.open(fileString);
+  //       final ref = FirebaseStorage.instance.ref().child(fileName);
+  //       uploadTask = ref.putFile(file);
+
+  //       uploadTask!.snapshotEvents.listen((event) {
+  //         setState(() {
+  //           _progress =
+  //               event.bytesTransferred.toDouble() / event.totalBytes.toDouble();
+  //           print(_progress.toString());
+  //         });
+  //         if (event.state == TaskState.success) {
+  //           _progress = null;
+  //           print(_progress);
+  //           //Fluttertoast.showToast(msg: 'File added to the library');
+  //         }
+  //       }).onError((error) {
+  //         // do something to handle error
+  //       });
+  //     });
+  //   } else {
+  //     fileString = "User canceled the picker.";
+  //     // User canceled the picker
+  //   }
+  // }
+
+  List<File>? files;
 
   void singleFile() async {
-    FilePickerResult? result = await FilePicker.platform.pickFiles();
+    FilePickerResult? result =
+        await FilePicker.platform.pickFiles(allowMultiple: true);
 
     if (result != null) {
-      File file = File(result.files.single.path!);
+      files = result.paths.map((path) => File(path!)).toList();
 
-      setState(() {
-        fileString = result.files.single.path!.toString();
-        //fileExtension = result.files.single.extension!.toString();
-        fileName = result.files.single.name.toString();
+      for (var file1 in files!) {
+        // print(file1);
+        // String file2 = file1.path;
+        //file2.lastIndexOf("/");
+        String fileName =
+            file1.path.toString().substring(file1.path.lastIndexOf("/") + 1);
         print(fileName);
-        // OpenFile.open(fileString);
         final ref = FirebaseStorage.instance.ref().child(fileName);
-        uploadTask = ref.putFile(file);
+         ref.putFile(file1);
+      }
+    } else {}
 
-        uploadTask!.snapshotEvents.listen((event) {
-          setState(() {
-            _progress =
-                event.bytesTransferred.toDouble() / event.totalBytes.toDouble();
-            print(_progress.toString());
-          });
-          if (event.state == TaskState.success) {
-            _progress = null;
-            print(_progress);
-            //Fluttertoast.showToast(msg: 'File added to the library');
-          }
-        }).onError((error) {
-          // do something to handle error
-        });
-      });
-    } else {
-      fileString = "User canceled the picker.";
-      // User canceled the picker
-    }
+    setState(() {});
   }
 
   @override
@@ -64,12 +88,13 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-          _progress !=null ? CircularProgressIndicator(
-              value: _progress,  //controller.value,
-              semanticsLabel: 'Linear progress indicator',
-            ): const SizedBox(height: 10),
-           
-            Text("Progress: $_progress"),
+            // _progress != null
+            //     ? CircularProgressIndicator(
+            //         value: _progress, //controller.value,
+            //         semanticsLabel: 'Linear progress indicator',
+            //       )
+            //     : const SizedBox(height: 10),
+            // Text("Progress: $_progress"),
             TextButton(
                 onPressed: () {
                   singleFile();
